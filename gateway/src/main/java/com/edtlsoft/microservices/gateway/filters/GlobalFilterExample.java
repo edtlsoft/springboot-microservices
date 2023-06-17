72169bbb9f703b9358fea7ef13e4ac243021c2a7
Edward Trigos <edward.trigos@nivelics.co>
2023-06-16 20:48:32 -0500

    [add] gateway service

---
+++ gateway/src/main/java/com/edtlsoft/microservices/gateway/filters/GlobalFilterExample.java
@@ -0,0 +1,40 @@
+package com.edtlsoft.microservices.gateway.filters;
+
+import org.slf4j.Logger;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GlobalFilterExample implements GlobalFilter, Ordered {
+
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(GlobalFilterExample.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Executing Global Filter Pre");
+
+        // modify request
+        exchange.getRequest().mutate().headers(h -> h.add("token", "1234567890"));
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            logger.info("Executing Global Filter Post");
+
+            String token = exchange.getRequest().getHeaders().getFirst("token");
+            logger.info("token: " + token);
+
+            exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "blue").build());
+            // exchange.getResponse().getHeaders().add("Content-Type", "text/plain");
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+
+}
