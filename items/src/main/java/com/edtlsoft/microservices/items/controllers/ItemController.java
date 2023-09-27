786e18f55d2a5d1e8768abc0911302b35cce5885
Edward Trigos <edward.trigos@nivelics.co>
2023-09-27 16:35:53 -0500

    [add] circuit breaker with decorators

--- items/src/main/java/com/edtlsoft/microservices/items/controllers/ItemController.java
+++ items/src/main/java/com/edtlsoft/microservices/items/controllers/ItemController.java
@@ -4,6 +4,8 @@ import com.edtlsoft.microservices.items.entities.Item;
 import com.edtlsoft.microservices.items.entities.Product;
 import com.edtlsoft.microservices.items.services.IItemService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class ItemController {
@@ -46,6 +49,20 @@ public class ItemController {
             );
     }
 
+    @CircuitBreaker(name="items", fallbackMethod = "fallback")
+    @GetMapping("show/{id}/quantity/{quantity}")
+    public Item show2(@PathVariable Long id, @PathVariable Integer quantity) {
+        logger.info("Method show2:::::::::::::::::::::::::::::::::::");
+        return itemService.findById(id, quantity);
+    }
+
+    @TimeLimiter(name="items", fallbackMethod = "fallback2")
+    @GetMapping("show3/{id}/quantity/{quantity}")
+    public CompletableFuture<Item> show3(@PathVariable Long id, @PathVariable Integer quantity) {
+        logger.info("Method show3:::::::::::::::::::::::::::::::::::");
+        return CompletableFuture.supplyAsync(() -> itemService.findById(id, quantity));
+    }
+
     public Item fallback(Long id, Integer quantity, Throwable throwable) {
         System.out.println("Fallback");
         logger.info(throwable.getMessage());
@@ -60,4 +77,21 @@ public class ItemController {
         item.setQuantity(quantity);
         return item;
     }
+
+    public CompletableFuture<Item> fallback2(Long id, Integer quantity, Throwable throwable) {
+        System.out.println("Fallback");
+        logger.info(throwable.getMessage());
+        Product product = new Product();
+        product.setId(id);
+        product.setName("Product default");
+        product.setPrice(50.00);
+        product.setPort(0);
+
+        Item item = new Item();
+        item.setProduct(product);
+        item.setQuantity(quantity);
+
+        return CompletableFuture.supplyAsync(() -> item);
+    }
+
 }
